33050f323bef8fd3a2f28eba5c219eebee1f07ff
Amin Saighi <amin.saighi@gmail.com>
2020-01-10 14:39:02 +0100

    dark mode
    Dark mode ON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,4 +42,24 @@ export class AppComponent {
     this.translate.use(this.language);
   }
 
+  darkMode() {
+    var checkbox = document.querySelector('input[name=theme]');
+
+    checkbox.addEventListener('change', function () {
+      if (this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+      } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+      }
+    })
+
+    let trans = () => {
+      document.documentElement.classList.add('transition');
+      window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+      }, 1000)
+    }
+  }
 }
